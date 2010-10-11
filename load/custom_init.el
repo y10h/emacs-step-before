dada9e21daf6d096b78d82fb6ffbd16b0a68c0e4
Yury Yurevich <yyurevich@jellycrystal.com>
2010-10-11 19:32:33 +0700

    Use 14px Monaco as default font.

--- load/custom_init.el
+++ load/custom_init.el
@@ -12,5 +12,6 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :weight normal :height 140 :width normal :foundry "apple" :family "Monaco")))) 
  '(fixed-pitch ((t (:foreground "dark green"))))
  '(org-column ((t (:strike-through nil :underline nil :weight normal :height 120 :family "outline-unifont")))))
